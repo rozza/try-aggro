226865cbbdd9eef59128fb4ec8acbdb7612623fb
A. Jesse Jiryu Davis <jesse@10gen.com>
2012-09-19 18:49:07 -0400

    Add server.py and main.html

---
+++ handlers.py
@@ -0,0 +1,5 @@
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('main.html')
---
+++ server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+
+import tornado.ioloop
+import tornado.web
+from tornado.web import StaticFileHandler, URLSpec
+from tornado import httpserver
+
+from handlers import *
+
+try:
+    import motor
+except ImportError:
+    print >> sys.stderr, (
+        "Can't import motor.\n\n"
+        " Motor is an experimental async driver for"
+        " MongoDB, get it by cloning\n"
+        " git://github.com/ajdavis/mongo-python-driver.git"
+        " and switching to branch 'motor',\n"
+        " then put the mongo-python-driver directory"
+        " on your PYTHONPATH\n\n"
+        )
+
+    raise
+
+
+if __name__ == "__main__":
+    db = motor.MotorConnection().open_sync().agg
+    static_path = 'static'
+
+    application = tornado.web.Application([
+        URLSpec(r"theme/static/(.+)", StaticFileHandler, {"path": static_path}, name='theme-static'),
+        URLSpec(r"/?", MainHandler, name='main'),
+        ],
+        db=db,
+        template_path='templates'
+    )
+
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(8000)
+    print 'Listening on port %s' % 8000
+    tornado.ioloop.IOLoop.instance().start()
